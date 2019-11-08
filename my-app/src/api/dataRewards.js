5028c34d24c25e61eaa84d8f9e060658407ba026
Caleb Davis <calebldavis0@gmail.com>
2019-11-08 11:42:24 -0500

    most of the code for the data table.

---
+++ my-app/src/api/dataRewards.js
@@ -0,0 +1,85 @@
+export default function() {
+    // simulates data coming from a database.
+    return Promise.resolve(
+      [
+          {
+            custid: 1,
+            name: "Dave",
+            amt: 120,
+            transactionDt: "10-01-2019"
+          },
+          {
+            custid: 1,
+            name: "Dave",
+            amt: 75,
+            transactionDt: "10-15-2019"
+          },
+          {
+            custid: 1,
+            name: "Dave",
+            amt: 100,
+            transactionDt: "10-31-2019"
+          },
+          {
+            custid: 1,
+            name: "Dave",
+            amt: 55,
+            transactionDt: "11-01-2019"
+          },
+          {
+            custid: 1,
+            name: "Dave",
+            amt: 75,
+            transactionDt: "11-24-2019"
+          },
+          {
+            custid: 1,
+            name: "Dave",
+            amt: 200,
+            transactionDt: "11-27-2019"
+          },
+          {
+            custid: 1,
+            name: "Dave",
+            amt: 15,
+            transactionDt: "10-08-2019"
+          },
+          {
+            custid: 1,
+            name: "Dave",
+            amt: 80,
+            transactionDt: "11-05-2019"
+          },
+          {
+            custid: 1,
+            name: "Dave",
+            amt: 220,
+            transactionDt: "11-21-2019"
+          },
+          {
+            custid: 1,
+            name: "Dave",
+            amt: 55,
+            transactionDt: "12-01-2019"
+          },
+          {
+            custid: 1,
+            name: "Dave",
+            amt: 15,
+            transactionDt: "12-08-2019"
+          },
+          {
+            custid: 1,
+            name: "Dave",
+            amt: 80,
+            transactionDt: "12-05-2019"
+          },
+          {
+            custid: 1,
+            name: "Dave",
+            amt: 220,
+            transactionDt: "12-21-2019"
+          }
+      ]
+    );
+  };
